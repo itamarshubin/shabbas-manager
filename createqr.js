6d92bdab751fab9ce02de107d33f6b8a925251fb
Itamar Shubin <itamarshubin@gmail.com>
2023-07-04 22:06:35 +0300

    add some logs

---
+++ createqr.js
@@ -0,0 +1,3 @@
+const qrcode =require('qrcode-terminal')
+
+qrcode.generate("2@56ud2rZCPCvt1O9cqbmAuEu28iLTNtmeiLd8Y0DIoc9p9Jgk4cPbfcK1rqZsO4WiuOUlFY0acUcQKg==,/LdJ8Xksnr89HzcKpvuO9PQfmrkB2/Z5PJIR1Mmr40Y=,GIUSI2C1jna3LY4OiIEwON9i/cgXy5KkJJjrdl1jEAQ=,aj15VqOO3xcfnzxmw1nGwFXOIdECTo4nkAao4wn5NcE=,1",{small:true})
