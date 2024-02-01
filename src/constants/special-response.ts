54242fc1634a1a3777f2efca45e24d6c05ffc2dc
Itamar Shubin <itamarshubin@gmail.com>
2024-02-02 01:08:05 +0200

    better manage for the special messages

--- src/constants/special-response.ts
+++ src/constants/special-response.ts
@@ -1,18 +1,19 @@
 import fs from "fs";
 import { Client } from "whatsapp-web.js";
 
-const specialMessages = fs.readFile(
-  "src/special-messages.json",
+let specialMessages:any;
+ fs.readFile(
+  "special-messages.json",
   "utf-8",
   (err, data) => {
     if (err) return null;
 
-    return JSON.parse(data);
+    specialMessages= JSON.parse(data);
   }
 );
 
-console.log(specialMessages);
 
-const sendSpecialMessages = (from: string, client: Client) => {
-//   if (specialMessages[from]) client.sendMessage(from, specialMessages[from]);
+export const sendSpecialMessages = (from: string, client: Client) => {
+  console.log(specialMessages);
+  if (specialMessages[from]) client.sendMessage(from, specialMessages[from]);
 };
--- src/message-handler.ts
+++ src/message-handler.ts
@@ -1,4 +1,4 @@
-import { Buttons, List, Message } from "whatsapp-web.js";
+import { Buttons, Client, List, Message } from "whatsapp-web.js";
 import { auth, isAdmin } from "./firebase/authantication";
 import { client } from "./app";
 import {
@@ -14,17 +14,10 @@ import {
   sessionedSubscribers,
 } from "./firebase/shabbas-manage";
 import { HELP_MESSAGE } from "./constants/help-message";
-// import { sendSpecialMessages } from "./constants/special-response";
+import { sendSpecialMessages } from "./constants/special-response";
 
 export const messageHandler = async (msg: Message) => {
-
-  // sendSpecialMessages(msg.from,client)
-  if (msg.from === "972587943119@c.us") {
-    client.sendMessage("972587943119@c.us", "עומו❤️");
-  }
-  if (msg.from === "972529060306@c.us") {
-    client.sendMessage("972529060306@c.us", "אבוששש❤️❤️❤️❤️❤️");
-  }
+  sendSpecialMessages(msg.from, client);
 
   if (msg.body.startsWith("!new") && (await isAdmin(msg))) {
     await addShabbas(msg);
